357b8b62b99e40a37ac44937fef67b81ebcce4a3
Jasper <jaspervanthuijl@gmail.com>
2019-03-13 15:54:19 +0100

    GitHub login front-end

---
+++ resources/js/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <login v-if="authenticatedUser === null" @authenticated="userAuthenticated"></login>
+        <div v-else class="row">
+            <div class="col-6">
+                <h1>Your favorites</h1>
+                <repository-list :user="authenticatedUser"></repository-list>
+            </div>
+            <div class="col-6">
+                <repository-comment></repository-comment>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Login from './Login';
+    import RepositoryList from './RepositoryList';
+    import RepositoryComment from './RepositoryComment';
+
+    export default {
+        name: 'App',
+        components: {Login, RepositoryList, RepositoryComment},
+        data() {
+            return {
+                authenticatedUser: null
+            }
+        },
+        methods: {
+            userAuthenticated(user) {
+                this.authenticatedUser = user;
+            }
+        }
+    }
+</script>
