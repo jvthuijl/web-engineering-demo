552cec78af0e287f2999a5487904a8f1b1df21e7
Jasper <jaspervanthuijl@gmail.com>
2019-03-13 13:20:55 +0100

    added basic vue things

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,11 @@
  */
 
 require('./bootstrap');
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 window.Vue = require('vue');
+Vue.use(BootstrapVue);
 
 /**
  * The following block of code may be used to automatically register your
@@ -17,13 +20,13 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+Vue.component('repository-list', require('./components/RepositoryListComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
+ *
+ * app Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
---
+++ resources/js/components/RepositoryListComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields" />
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        // Note `isActive` is left out and will not appear in the rendered table
+        fields: ['repository', 'user', 'number_of_stars'],
+        items: [
+            {repository: 'Test', user: 'Jasper', number_of_stars: 10},
+            {repository: 'Test', user: 'Julius', number_of_stars: 10},
+            {repository: 'Test', user: 'Noam', number_of_stars: 10}
+        ]
+      }
+    }
+  }
+</script>
