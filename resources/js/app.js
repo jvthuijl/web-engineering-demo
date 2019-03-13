8c7025a3ee34ca65d0a14b368bb075832fd61c37
Julius <julius@deboprojects.nl>
2019-03-13 16:09:53 +0100

    Fix CSS loading on Windows

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 
 require('./bootstrap');
 import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './components/App';
 
 window.Vue = require('vue');
